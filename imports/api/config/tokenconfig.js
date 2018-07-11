e3c474f49b82a7e0ecad5d4eec6b5e107433d692
lLuis <lgarciava.fhc@gmail.com>
2018-07-11 10:21:18 -0400

    modified:   .desktop/assets/meteor.icns 	modified:   .desktop/assets/meteor.ico 	modified:   .desktop/assets/meteor.png 	modified:   .desktop/assets/splashScreen.png 	modified:   .meteor/.finished-upgraders 	modified:   .meteor/packages 	modified:   .meteor/release 	modified:   .meteor/versions 	modified:   imports/api/config/tokenconfig.js 	new file:   npm-debug.log 	modified:   package-lock.json 	modified:   private/static/LINUX/marketmaker 	modified:   private/static/OSX/marketmaker 	modified:   private/static/WIN/libcurl.dll 	modified:   private/static/WIN/marketmaker.exe 	modified:   private/static/WIN/msvcp140.dll 	modified:   private/static/WIN/nanomsg.dll 	modified:   private/static/WIN/vcruntime140.dll

--- imports/api/config/tokenconfig.js
+++ imports/api/config/tokenconfig.js
@@ -4,7 +4,7 @@ let tokenconfig = {
         shortcode: 'GLXT',
         siteurl: 'https://glx.co',
         supporturl: 'https://glx.co',
-        pricebob: '0x_blalbal',
+        pricebob: 'b30ab074d57e2aa0f639ae9bc5965792ceb6270c64561a612e421c43b2a4fd13',
         netid: 0,
         seed: '0.0.0.0'
     }
