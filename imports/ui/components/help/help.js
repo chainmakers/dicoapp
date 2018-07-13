ceaed9026654cad4eca6747e8058db36b79ea9e1
pbca26 <pbca26@gmail.com>
2018-07-13 23:45:00 +0300

    help links render

--- imports/ui/components/help/help.js
+++ imports/ui/components/help/help.js
@@ -1,1 +1,6 @@
 import './help.html';
+import helpLinks from '/imports/ui/helpers/helpLinks';
+
+Template.registerHelper('getLinks', function() {
+  return helpLinks;
+});
---
+++ imports/ui/helpers/helpLinks.js
@@ -0,0 +1,9 @@
+const helpLinks = [{
+  title: 'Slack',
+  url: 'http://slack.com',
+}, {
+  title: 'Website',
+  url: 'http://somewebsitehere',
+}];
+
+export default helpLinks;
