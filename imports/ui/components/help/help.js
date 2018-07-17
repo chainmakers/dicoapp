4318ba94e60de3f233ed0e2bd606c8ee1428fbbb
ca333 <ca333@safe-mail.net>
2018-07-17 10:11:53 +0200

    modify UI for chainmakers/GLX

--- imports/ui/components/help/help.js
+++ imports/ui/components/help/help.js
@@ -1,6 +1,11 @@
 import './help.html';
 import helpLinks from '/imports/ui/helpers/helpLinks';
+import tokenconfig from '/imports/api/config/tokenconfig.js';
 
 Template.registerHelper('getLinks', function() {
   return helpLinks;
+});
+
+Template.registerHelper('dICOToken', function() {
+  return tokenconfig.dICOtoken.shortcode;
 });
